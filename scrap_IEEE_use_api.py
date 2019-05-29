b662dc5f176df3032798143af1cc36fa292d8e41
qiyu <qiyu_sjtu@163.com>
2019-05-29 10:04:10 +0800

    reconstruct functions and add functions of downloading

--- scrap_IEEE_use_api.py
+++ scrap_IEEE_use_api.py
@@ -3,75 +3,90 @@ import xplore
 import json
 from colorama import Fore, Back, Style,init
 import os
+import requests
+import re
 
-def query_by_text(text,query):
-    query.queryText(text)
-    # data = query.callAPI()['articles'][0]
-    data = query.callAPI()
-    total_num = data['total_records']
-    articles = data['articles']
-    # js = json.dumps(articles, sort_keys=True, indent=4, separators=(',', ':'))
-    # print(js)
-    print('Totally '+str(total_num)+' articles')
-    for item in articles:
-        url = ''
-        title = item['title']
-        citing = item['citing_paper_count']
-        publication_title = item['publication_title']
-        year = item['publication_year']
-        if 'html_url' in item.keys():
-            url = item['html_url']
-        authors = []
-        for author in item['authors']['authors']:
-            authors.append(author['full_name'])
-        parsed_author = ','.join(authors)
-        # print(Fore.RED + 'some red text')   
-        try:
-            print(Fore.YELLOW+title+Style.RESET_ALL)
-            print('------------------')
-            print(parsed_author)
-            print(str(year)+' '+publication_title)
-            print('cite: '+str(citing))
-            print(url)
-            print('')
-        except:
-            continue
-    # print(js)
-    # for item in data:
-    #     js = json.dumps(item, sort_keys=True, indent=4, separators=(',', ':'))
-    #     print(js)
-        # try:
-        #         print(item['title'])
-        #         print(item['author'])
-        # except:
-        #     continue
-    # print(json.dumps(data,sort_keys=True, indent=2))
-def get_number(doi):
-    number = 0
-    # try:
-    try:
-        data = query.doi(doi)
-        query.dataType('JSON')
-        query.dataFormat('object')
-        data = query.callAPI()
-        data = data['articles'][0]
-        title = data['title']
-        number = data['article_number']
-    except Error as e:
-        print(e)
-    return number,title
+class IEEEXPLORE():
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0'}
+        self.key = '6rcf2h3cjuxy9c2m9zk9mh8e'
+        self.query = xplore.xploreapi.XPLORE(self.key)
+        self.query.dataType('JSON')
+        self.query.dataFormat('object')
+        self.query.resultSetMax = 2
+        self.item_per_turn = 1
+
+    def query_by_text(self,text):
+        self.query.queryText(text)
+        # data = query.callAPI()['articles'][0]
+        data = self.query.callAPI()
+        total_num = data['total_records']
+        articles = data['articles']
+        # js = json.dumps(articles, sort_keys=True, indent=4, separators=(',', ':'))
+        # print(js)
+        print('Totally '+str(total_num)+' articles')
+        self.list_articles(articles)
+        
+    def list_articles(self,articles):
+        turns = len(articles)/(self.item_per_turn)
+        for i in range(turns):
+            for item in articles[i*self.item_per_turn:(i+1)*self.item_per_turn]:
+                url = ''
+                title = item['title']
+                citing = item['citing_paper_count']
+                publication_title = item['publication_title']
+                year = item['publication_year']
+                number = item['is_number']
+                authors = []
+                for author in item['authors']['authors']:
+                    authors.append(author['full_name'])
+                parsed_author = ','.join(authors)
+                if('pdf_url' in item.keys()):
+                    url = item['pdf_url']
+                    number = re.findall(r'.*?=(\d+)',url)[0]
+                # print(Fore.RED + 'some red text')
+                try:
+                    print(Fore.YELLOW+title+Style.RESET_ALL)
+                    print('------------------')
+                    print(parsed_author)
+                    print(str(year)+' '+publication_title)
+                    print('cite: '+str(citing))
+                    print(number)
+                    print(url)
+                    print('')
+                except:
+                    continue
+                down_flag = raw_input('download?')
+                if(down_flag == 'no'):
+                    continue
+                else:
+                    title = title.replace('/',' ')
+                    
+                    self.download_pdf(number,title)
+    def download_pdf(self,num,title):
+        title = title.replace('"','')
+        url = 'http://ieeexplore.ieee.org/stampPDF/getPDF.jsp?tp=&isnumber=&arnumber='+str(num)
+        filename = title+'.pdf'
+        # filename = 'ts.pdf'
+        print('Start downloading "'+title+'"...')
+        with open(filename,'wb') as f:
+            f.write(self.session.get(url).content)
+
+        print('Complete')
+
+    def test_download(self,num):
+        url = 'http://ieeexplore.ieee.org/stampPDF/getPDF.jsp?tp=&isnumber=&arnumber='+str(num)
+        with open('test.pdf','wb') as f:
+            f.write(self.session.get(url).content)
+
 
 if __name__ == '__main__':
+    ieee = IEEEXPLORE()
     init()
-    key = '6rcf2h3cjuxy9c2m9zk9mh8e'
-    query = xplore.xploreapi.XPLORE(key)
-    # query.maximumResults(200)
-    query.dataType('JSON')
-    query.dataFormat('object')
-    query.resultSetMax = 20
     while(1):
         text = raw_input('Input query text(Input "exit" to close): ')
         if(text=='exit'):
             break
-        query_by_text(text,query)
+        ieee.query_by_text(text)
     os.system('pause')
